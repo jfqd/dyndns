25ede7ce4f1501f99ab270f4bd925241eca17ba0
jfqd <jfqd@blun.org>
2016-04-12 12:02:12 +0200

    ensure a valid database connection, reconnect if missing

--- app.rb
+++ app.rb
@@ -46,6 +46,7 @@ end
 
 get '/update/:token/:domain' do
   begin
+    ActiveRecord::Base.verify_active_connections!
     halt 403 if params[:token].blank? || params[:domain].blank?
     halt 403 if !params[:domain].match(domain_regex)
     t = AuthToken.find_by_token(params[:token])
